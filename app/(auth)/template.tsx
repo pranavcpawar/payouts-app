30cbd13448657ba8bdae9aa8e89acd1d145b8095
pranavcpawar <pranavcpawar@outlook.com>
2025-01-17 00:55:53 +0530

    add loader to handle hydration

--- app/(auth)/template.tsx
+++ app/(auth)/template.tsx
@@ -1,7 +1,24 @@
-export default async function Template({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+"use client";
+
+import { LoaderCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setIsLoading(false);
+    }, 500);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-[90%] flex items-center justify-center">
+        <LoaderCircle className="h-8 w-8 animate-spin" />
+      </div>
+    );
+  }
+
   return <div className="w-full h-full">{children}</div>;
 }
