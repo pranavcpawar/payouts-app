f77fbb0ec4e068393a961371bb97763c10d16c7c
pranavcpawar <pranavcpawar@outlook.com>
2025-01-17 00:53:58 +0530

    add registration form

---
+++ app/(auth)/signup/cred-signup.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoaderButton } from "@/components/loader-button";
+
+const registrationSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid email" }),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must be 8 characters long!"),
+    passwordConfirmation: z
+      .string()
+      .min(1, "Confirm Password is required")
+      .min(8, "Confirm Password must be 8 characters long!"),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords don't match",
+    path: ["passwordConfirmation"],
+  });
+
+export function RegistrationForm() {
+  const form = useForm<z.infer<typeof registrationSchema>>({
+    resolver: zodResolver(registrationSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof registrationSchema>) {
+    console.log(data);
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="w-full"
+                  placeholder="turing@example.com"
+                  type="email"
+                  autoComplete="off"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="w-full"
+                  placeholder="●●●●●●●●●●●●●"
+                  type="password"
+                  autoComplete="off"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="passwordConfirmation"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="w-full"
+                  placeholder="●●●●●●●●●●●●●"
+                  type="password"
+                  autoComplete="off"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <LoaderButton isLoading={false} className="w-full" type="submit">
+          Sign up
+        </LoaderButton>
+      </form>
+    </Form>
+  );
+}
